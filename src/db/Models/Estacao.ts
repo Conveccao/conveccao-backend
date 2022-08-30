21b9ed92835304024c6ab55202ede335d414343f
gustavols <gustavoleandro616@gmail.com>
2022-08-30 08:56:57 -0300

    iniciando criacao da funcao de get estacoes

---
+++ src/db/Models/Estacao.ts
@@ -0,0 +1,29 @@
+export default class Estacao{
+    private _id: number;
+    private _lat: number;
+    private _lon: number;
+    private _reference: string;
+
+    public constructor (id: number, lat: number, lon: number, reference: string){
+        this._id = id;
+        this._lat = lat;
+        this._lon = lon;
+        this._reference = reference;
+    }
+
+    public getId = (): number => {
+        return this._id;
+    }
+
+    public getLat = (): number => {
+        return this._lat;
+    }
+
+    public getLon = (): number => {
+        return this._lon;
+    }
+
+    public getReference = (): string => {
+        return this._reference;
+    }
+}
