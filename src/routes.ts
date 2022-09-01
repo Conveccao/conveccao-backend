94e425a9ec81dd43b7904b5a2ed3692fc51a28d0
Daniel <daniel.2308@outlook.com>
2022-09-01 17:29:19 -0300

    feat: Adding delete-stations function

--- src/routes.ts
+++ src/routes.ts
@@ -6,5 +6,5 @@ const routes = Router()
 routes.get('/get-stations', new StationControllers().get)
 routes.get('/get-stations/:id', new StationControllers().getById)
 routes.post('/stations', new StationControllers().create)
-
+routes.delete('/delete-stations/:id', new StationControllers().delete)
 export default routes
