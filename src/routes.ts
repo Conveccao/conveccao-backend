b451900e6d054938c53e7cdcd831f6db3edfbc0c
Daniel <daniel.2308@outlook.com>
2022-09-08 12:07:24 -0300

    refactor: Alteração do nome das rotas

--- src/routes.ts
+++ src/routes.ts
@@ -5,20 +5,20 @@ import { StationControllers } from './controllers/stationControllers'
 
 const routes = Router()
 
-routes.get('/get-stations', new StationControllers().get)
-routes.get('/get-stations/:id', new StationControllers().getById)
+routes.get('/stations', new StationControllers().get)
+routes.get('/stations/:id', new StationControllers().getById)
 routes.post('/stations', new StationControllers().create)
-routes.delete('/delete-stations/:id', new StationControllers().delete)
-routes.put('/update-station/:id', new StationControllers().update)
+routes.delete('/stations/:id', new StationControllers().delete)
+routes.put('/stations/:id', new StationControllers().update)
 
-routes.post('/sensorType', new SensorTypeControllers().create)
-routes.get('/get-sensorsType', new SensorTypeControllers().get)
+routes.post('/sensorTypes', new SensorTypeControllers().create)
+routes.get('/sensorTypes', new SensorTypeControllers().get)
 
 
-routes.post('/sensor', new SensorControllers().create)
-routes.get('/get-sensors', new SensorControllers().get)
-routes.get('/get-sensor/:id', new SensorControllers().getById)
-routes.delete('/delete-sensor/:id', new SensorControllers().delete)
-routes.put('/put-sensor/:id', new SensorControllers().put)
+routes.post('/sensors', new SensorControllers().create)
+routes.get('/sensors', new SensorControllers().get)
+routes.get('/sensors/:id', new SensorControllers().getById)
+routes.delete('/sensors/:id', new SensorControllers().delete)
+routes.put('/sensors/:id', new SensorControllers().put)
 
 export default routes
