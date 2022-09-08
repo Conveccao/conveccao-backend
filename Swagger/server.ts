7064281ad140efc1ac229e7e30a565506a31dbed
Daniel <daniel.2308@outlook.com>
2022-09-08 10:39:39 -0300

    fix: Consertando rotas do Swagger

--- Swagger/server.ts
+++ Swagger/server.ts
@@ -1,7 +1,8 @@
 import express from "express"
 import swaggerUI from "swagger-ui-express"
+import { AppDataSource } from "../src/data-source";
 
-import { router } from "./routes";
+import routes from "../src/routes";
 
 import swaggerDocs from "./swagger.json";
 
@@ -17,5 +18,19 @@ app.get("/terms", (request, response) => {
    });
 });
 
-app.use("/v1", router);
-app.listen(3000, () => console.log("Server is running on port 3000"))
+AppDataSource.initialize().then(() => {
+    const app = express()
+
+    app.use(express.json())
+
+    app.use(routes)
+
+    //app.get('/', (req, res) => {
+    //    return res.json('ok')
+    //})
+
+    return app.listen(process.env.PORT)
+})
+
+app.use("/v1", routes);
+app.listen(3001, () => console.log("Server is running on port 3000"))
