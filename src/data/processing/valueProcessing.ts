7d061b9fb73474b964c4b477ad0c5b403bfe848b
Daniel <daniel.2308@outlook.com>
2022-11-27 14:08:38 -0300

    Finalização da primeira versão da função de alertas

--- src/data/processing/valueProcessing.ts
+++ src/data/processing/valueProcessing.ts
@@ -10,25 +10,25 @@ import { AlertService } from '../../services/alertService';
 
 export default async function ValueProcessing(value: IValue) {
     const station = await stationRepository.findOne({
-        where:{
+        where: {
             id: value.station_id
         },
-        relations:{
+        relations: {
             parameters: true
         }
     })
     const parameterTypes = await parameterTypeRepository.find()
     let parametersList
-    if(station != null){
+    if (station != null) {
 
-        if(station.parameters != undefined){
+        if (station.parameters != undefined) {
             parametersList = await newParameterList(station.parameters)
         }
 
         parametersList?.forEach(parameter => {
             let parameterType = parameter.parameterType
-            let typeValue = verifyParameterType(parameterType, value)
-            if(typeValue != null){
+            let typeValue = verifyParameterType(parameterType, value, station.name)
+            if (typeValue != null) {
                 createMeasurement(parameter, typeValue, value.unixtime)
             }
         })
@@ -38,29 +38,59 @@ export default async function ValueProcessing(value: IValue) {
     }
 }
 
-function verifyParameterType(parameterType: ParameterType, value: IValue){
+function verifyParameterType(parameterType: ParameterType, value: IValue, station_name: string) {
     let type = parameterType.reference
     const alertService = new AlertService()
-    if(type === "temp" && value.temp != null) {
-        if(value.temp > 33){
-            let tempAlertDto = new AlertDto("Temperatura muito alta", "Campos do Jordão", "22-11-2022", "11:11")
-            alertService.createAlert(tempAlertDto)
-        } else if (value.temp < 10){
-            let tempAlertDto = new AlertDto("Temperatura muito baixa", "Campos do Jordão", "29-11-2022", "11:11")
-            alertService.createAlert(tempAlertDto)
-        }
+
+    if (type === "temp" && value.temp != null) { //33 e 10
+        verifyValues(value.temp, 33, 10, "Temperatura", station_name, alertService)
         return value.temp
-    } 
-    if(type === "umid" && value.umid != null) return value.umid
-    if(type === "vent" && value.vent != null) return value.vent
-    if(type === "pluv" && value.pluv != null) return value.pluv
-    if(type === "v_vent" && value.vvent != null) return value.vvent
-    if(type === "d_vent" && value.dvent != null) return value.dvent
+    }
+
+    if (type === "umid" && value.umid != null) {
+        verifyValues(value.temp, 75, 30, "Umidade", station_name, alertService)
+        return value.umid
+    }
+    if (type === "vent" && value.vent != null) return value.vent
+    if (type === "pluv" && value.pluv != null) {
+        verifyValuesPluv(value.pluv, station_name, alertService)
+        return value.pluv
+    }
+    if (type === "v_vent" && value.vvent != null) {
+        verifyValues(value.vvent, 0, 60, "Velocidade do vento", station_name, alertService)
+        return value.vvent
+    }
+    if (type === "d_vent" && value.dvent != null) return value.dvent
     return null
 }
 
+function verifyValues(value: number, min_value: number, max_value: number, parameterType_name: string, station_name: string, alert_service: AlertService) {
+    const date = new Date()
+    const formatedDate = formatDate(date)
+    const formatedHours = formatDateHours(date)
+    if (value > max_value) {
+        const alert = new AlertDto(`${parameterType_name} muito alta`, station_name, formatedDate, formatedHours)
+        alert_service.createAlert(alert)
+    } else if (value < min_value) {
+        const alert = new AlertDto(`${parameterType_name} muito baixa`, station_name, formatedDate, formatedHours)
+        alert_service.createAlert(alert)
+    }
+}
 
-async function newParameterList(parameters: Parameter[]){
+function verifyValuesPluv(value: number, station_name: string, alert_service: AlertService) {
+    const date = new Date()
+    const formatedDate = formatDate(date)
+    const formatedHours = formatDateHours(date)
+    if (value > 200) {
+        const alert = new AlertDto(`Quantidade de chuvas muito alta, risco de deslizamentos`, station_name, formatedDate, formatedHours)
+        alert_service.createAlert(alert)
+    } else if (value < 10) {
+        const alert = new AlertDto(`Quantidade de chuvas muito baixa`, station_name, formatedDate, formatedHours)
+        alert_service.createAlert(alert)
+    }
+}
+
+async function newParameterList(parameters: Parameter[]) {
     const allParams = await parameterRepository.find({
         relations: {
             parameterType: true
@@ -70,16 +100,40 @@ async function newParameterList(parameters: Parameter[]){
     let newParamList: Parameter[] = []
     allParams.forEach(param => {
         parameters.forEach(parameter => {
-            if(param.id == parameter.id){
+            if (param.id == parameter.id) {
                 newParamList.push(param)
             }
         })
     })
 
-    return newParamList   
+    return newParamList
 }
 
-async function createMeasurement(parameter: Parameter, value: number, date_time: number){
-    let Measurement = gaugedRepository.create({parameter, value, date_time})
+async function createMeasurement(parameter: Parameter, value: number, date_time: number) {
+    let Measurement = gaugedRepository.create({ parameter, value, date_time })
     await gaugedRepository.save(Measurement)
+}
+
+function padTo2Digits(num: number) {
+    return num.toString().padStart(2, '0');
+}
+
+function formatDate(date: Date) {
+    return (
+        [
+            padTo2Digits(date.getDate()),
+            padTo2Digits(date.getMonth() + 1),
+            date.getFullYear(),
+        ].join('-')
+    );
+}
+
+function formatDateHours(date: Date) {
+    return (
+        [
+            padTo2Digits(date.getHours()),
+            padTo2Digits(date.getMinutes()),
+            padTo2Digits(date.getSeconds()),
+        ].join(':')
+    )
 }
